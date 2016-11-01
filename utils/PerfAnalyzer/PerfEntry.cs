a7fca86713d5b187b3c6687d0c4919a0f56d775c
manisero <michalaniserowicz@gmail.com>
2016-11-02 00:38:05 +0100

    Implemented showing block durations

---
+++ utils/PerfAnalyzer/PerfEntry.cs
@@ -0,0 +1,48 @@
+﻿using CsvHelper.Configuration;
+
+namespace PerfAnalyzer
+{
+    public class PerfEntry
+    {
+        public string EventName { get; set; }
+
+        public double Timestamp { get; set; }
+
+        public string BlockName { get; set; }
+
+        public int ElapsedTicks { get; set; }
+    }
+
+    public class PerfEntryMap : CsvClassMap<PerfEntry>
+    {
+        public PerfEntryMap()
+        {
+            Map(m => m.EventName).Index(0).ConvertUsing(x => ParseEventName(x.GetField<string>(0)));
+            Map(m => m.Timestamp).Index(1).ConvertUsing(x => ParseTimestamp(x.GetField<string>(1)));
+            Map(m => m.BlockName).Index(3).ConvertUsing(x => ParseBlockName(x.GetField<string>(3)));
+            Map(m => m.ElapsedTicks).Index(4).ConvertUsing(x => ParseElapsedTicks(x.GetField<string>(4)));
+        }
+
+        private string ParseEventName(string value)
+        {
+            return value.Trim().Replace("Manisero.DataflowPoc/", string.Empty);
+        }
+
+        private double ParseTimestamp(string value)
+        {
+            return double.Parse(value.Replace(" ", string.Empty).Replace(new string((char)160, 1), string.Empty));
+        }
+
+        private string ParseBlockName(string value)
+        {
+            return value.Trim();
+        }
+
+        private static int ParseElapsedTicks(string value)
+        {
+            return !string.IsNullOrWhiteSpace(value)
+                       ? int.Parse(value.Replace(" ", string.Empty).Replace(new string((char)160, 1), string.Empty))
+                       : -1;
+        }
+    }
+}
--- utils/PerfAnalyzer/Program.cs
+++ utils/PerfAnalyzer/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace PerfAnalyzer
+﻿using System;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+
+namespace PerfAnalyzer
 {
     class Program
     {
+        private const string PERF_RESULT_PATH = @"..\..\..\..\perf_results\result.csv";
+
         static void Main(string[] args)
         {
+            var csvReader = GetCsvReader(PERF_RESULT_PATH);
+            var entries = csvReader.GetRecords<PerfEntry>().ToList();
+
+            var blockDurations = entries.Where(x => x.EventName == "BlockExit")
+                                        .GroupBy(x => x.BlockName)
+                                        .ToDictionary(x => x.Key, x => x.Sum(item => item.ElapsedTicks));
+
+            foreach (var duration in blockDurations)
+            {
+                Console.WriteLine($"{duration.Key}: {duration.Value} ticks");
+            }
+        }
+
+        private static CsvReader GetCsvReader(string perfResultPath)
+        {
+            var textReader = File.OpenText(perfResultPath);
+
+            var csvReader = new CsvReader(textReader);
+            csvReader.Configuration.Delimiter = "\t";
+            csvReader.Configuration.RegisterClassMap<PerfEntryMap>();
+
+            return csvReader;
         }
     }
 }
