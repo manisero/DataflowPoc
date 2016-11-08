821960b1632cb52c373b24a0c4590bc1bd6efe29
manisero <michalaniserowicz@gmail.com>
2016-11-08 20:48:43 +0100

    Used Dapper to query database

---
+++ src/Manisero.DataflowPoc.DataExporter/Domain/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Manisero.DataflowPoc.DataExporter.Domain
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public double ArmToLegLengthRatio { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
--- src/Manisero.DataflowPoc.DataExporter/Program.cs
+++ src/Manisero.DataflowPoc.DataExporter/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace Manisero.DataflowPoc.DataExporter
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using Dapper;
+using Manisero.DataflowPoc.DataExporter.Domain;
+
+namespace Manisero.DataflowPoc.DataExporter
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var connectionString = ConfigurationManager.ConnectionStrings["DataExporter"].ConnectionString;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var people = connection.Query<Person>(@"select * from Person")
+                                        .AsList();
+
+            }
+
+            // TODO:
+            // - read data (Dapper)
+            // - write to csv (CsvHelper)
         }
     }
 }
