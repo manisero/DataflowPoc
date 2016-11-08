20e3f9d733a6dfab5bcb3f70b9ec08493a82f2a4
manisero <michalaniserowicz@gmail.com>
2016-11-08 20:53:16 +0100

    Created database project

---
+++ src/Manisero.DataflowPoc.DataExporter.Database/Tables/Person.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Person] (
+    [PersonId]            INT             IDENTITY (1, 1) NOT NULL,
+    [FirstName]           NVARCHAR (100)  NOT NULL,
+    [LastName]            NVARCHAR (100)  NOT NULL,
+    [DateOfBirth]         DATE            NULL,
+    [ArmToLegLengthRatio] FLOAT (53)      NULL,
+    [PhoneNumber]         NVARCHAR (50)   NULL,
+    [Salary]              DECIMAL (19, 4) NULL,
+    CONSTRAINT [PK_Person] PRIMARY KEY CLUSTERED ([PersonId] ASC)
+);
+
